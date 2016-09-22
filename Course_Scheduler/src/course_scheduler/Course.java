c9529456ba42cec073e7e8bef8cd67f52b6f527d
Michael Vance <mykol00@gmail.com>
2016-09-22 15:30:51 -0500

    created course and teacher objects
    The file has been parsed, and now the data has been stored in the course
    and teacher class objects.

--- Course_Scheduler/src/course_scheduler/Course.java
+++ Course_Scheduler/src/course_scheduler/Course.java
@@ -10,10 +10,20 @@ package course_scheduler;
  * @author Myk
  */
 public class Course {
-    String name, time, department, building;
-    int classroom, enrollment;
+    String name, time, department, building, classroom;
+    int enrollment;
+    
+    public Course(String name) {
+        this.name = name;
+        this.department = name.split(" ")[0];
+    }
     
     public void test() {
         System.out.println("test");
+    }
+    
+    @Override
+    public String toString () {
+        return "department = " + department + "; time = " + time + "; building = " + building + "; classroom = " + classroom + "; name = " + name;
     }
 }
