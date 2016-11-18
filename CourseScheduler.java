729e10d3f1192eabc3a939af9e78e9d7759db7c8
Mykol00 <Mykol00@gmail.com>
2016-11-17 18:29:53 -0600

    COURSE SCHEDULER CLASS

--- CourseScheduler.java
+++ CourseScheduler.java
@@ -5,10 +5,41 @@
  */
 package course_scheduler_beta;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
 /**
  *
- * @author mv0003
+ * @author Myk
+ * @author Victoria
+ * 
+ * This class handles the scheduling process
  */
 public class CourseScheduler {
+    List timeSlots;
+    SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+    try{
+        Date startOfDay = timeFormat.parse("8:00:00");
+        Date endOfDay = timeFormat.parse("20:00:00");
+    }
+    catch (Exception e){
+        System.out.println("error doing time stuff");
+    }
     
+    public CourseScheduler(List<Classroom> rooms, int dur, int gap){
+        
+    }
+    
+    // create time slots (military time) based on duration of classes (dur), and time between classes (gap)
+    private void makeTimeSlots(int dur, int gap){
+        int totalClassTime = dur + gap;
+        int totalDayTime = 720;
+        int numberOfTimeSlots = totalDayTime/totalClassTime;
+        for(int i = 0; i < numberOfTimeSlots; i++){
+            //int slot = startOfDay + (totalClassTime * i);
+            //timeSlots.add(slot);
+        }
+    }
 }
