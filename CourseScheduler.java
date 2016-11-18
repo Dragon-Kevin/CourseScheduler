66b5002bcc27e16acb4d1e41d8507083463f9294
Mykol00 <Mykol00@gmail.com>
2016-11-17 18:28:32 -0600

    THE COURSE SCHEDULER CLASS

---
+++ CourseScheduler.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package course_scheduler_beta;
+
+/**
+ *
+ * @author mv0003
+ */
+public class CourseScheduler {
+    
+}
