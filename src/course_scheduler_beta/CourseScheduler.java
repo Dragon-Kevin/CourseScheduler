7facc34ac605b3afdf9e8bf4e0259b2405ee647a
Mykol00 <Mykol00@gmail.com>
2016-11-19 20:09:44 -0600

    Times are now in "3:30 pm" format.
    Re-upload; the first one ignored tuesday/thursday classes.

--- src/course_scheduler_beta/CourseScheduler.java
+++ src/course_scheduler_beta/CourseScheduler.java
@@ -142,29 +142,27 @@ public class CourseScheduler {
         courseMeetingTimes = new String[2*numberOfTimeSlots];
         
         
-        // For MW classes
+        // Find time slots
         for(int i = 0; i < numberOfTimeSlots; i++){
             String amORpm1 = " am";
+            String amORpm2 = " am";
             int slot1 = startOfDay + ((int)((totalClassTime * i)/60))*(100) + (totalClassTime * i)%60;;
             if(slot1%100 >= 60) slot1+=40;
-            if(slot1 >= 1200) { amORpm1 = " pm"; slot1-=1200;}
+            if(slot1 >= 1200) { amORpm1 = " pm"; amORpm2 = " pm"; slot1-=1200;}
             
-            String amORpm2 = " am";
+            
             int slot2 = slot1 + ((int)((totalClassTime)/60))*(100) + ((totalClassTime)%60);
             if(slot2%100 >= 60) slot2+=40;
             if(slot2 >= 1200) {amORpm2 = " pm"; slot2-=1200; }
             
-            courseMeetingTimes[i] = String.valueOf(slot1) + amORpm1 + " - " + String.valueOf(slot2) + amORpm2 + " MW ";
-            System.out.println(String.format("%d:%02d %s - %d:%02d %s MW",(int)(slot1/100),slot1%100, amORpm1, (int)(slot2/100),slot2%100, amORpm2));
+            // the top one is for monday/wednesday classes, bottom is for tuesday/thursday classes.
+            courseMeetingTimes[i] = String.format("%d:%02d %s - %d:%02d %s MW",(int)(slot1/100),slot1%100, amORpm1, (int)(slot2/100),slot2%100, amORpm2);
+            courseMeetingTimes[i + numberOfTimeSlots] = String.format("%d:%02d %s - %d:%02d %s TR",(int)(slot1/100),slot1%100, amORpm1, (int)(slot2/100),slot2%100, amORpm2);
         }
         
-        // For TR classes
-        for(int i = 0; i < numberOfTimeSlots; i++){
-            int slot = startOfDay + (totalClassTime * i);
-            courseMeetingTimes[i + numberOfTimeSlots] = String.valueOf(slot) + " - " + String.valueOf(slot + totalClassTime) + " TR ";
-        }
-        //for(String ele:courseMeetingTimes)
-            //System.out.println(ele);
+        /* Myk: Prints out the courseMeetingTimes array*/ 
+        for(String ele:courseMeetingTimes)
+            System.out.println(ele);
     }
     
     /* Modified by Victoria Mitchell */
