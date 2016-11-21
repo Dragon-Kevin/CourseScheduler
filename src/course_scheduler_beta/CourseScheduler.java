6ccb260403b91216aadbbfc37f5b8f95069a4878
Mykol00 <Mykol00@gmail.com>
2016-11-20 22:07:32 -0600

    Assigning Teachers based on preference works*
    *Doesn't consider the fact that one teacher can only be teaching one class per time period.

--- src/course_scheduler_beta/CourseScheduler.java
+++ src/course_scheduler_beta/CourseScheduler.java
@@ -6,17 +6,12 @@
 package course_scheduler_beta;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JFileChooser;
 
 /**
  *
@@ -53,8 +48,8 @@ public class CourseScheduler {
         List<Classroom> classrooms = db.getClassrooms(null, null);
         
         // Make the time slots for assigning classes
-        makeTimeSlots();
-        teacherPrefs(db);
+        makeTimeSlots(classrooms);
+        teacherPrefs(db, classrooms);
         /* Handle the classroom preferences first */
 //        assignClassroomPreferences(courses, preferences, teachers, courseMeetingTimes, classrooms);
         
@@ -71,7 +66,7 @@ public class CourseScheduler {
                 for (Teacher iTeacher : teachers){
                     for (String teacherCourse : iTeacher.teachableCourses){
                         if (iCourse.name.equalsIgnoreCase(teacherCourse)) {
-                            iCourse.setProf(iTeacher.getName());
+                            iCourse.setProf(iTeacher.name);
                         }                
                     }
                 }*/
@@ -134,7 +129,7 @@ public class CourseScheduler {
         }
     }
     
-    private void teacherPrefs(DatabaseUtility db) {
+    private void teacherPrefs(DatabaseUtility db, List<Classroom> rooms) {
         List<Teacher> t = new ArrayList();
         t.addAll(db.getProfessors("TIME_PREF","Evening"));
         t.addAll(db.getProfessors("TIME_PREF","Morning"));
@@ -149,29 +144,61 @@ public class CourseScheduler {
             //if(checkTime(i.getTimePreference()))
             List<String> possibleTimes = matchPref(i.getTimePreference());
             Parser.printList(possibleTimes);
+            for(Course y:c){
+                //if(y.getClassroom().length() > 0){
+                    /*for(String p: possibleTimes){
+                        if(y.getClassroom_().isSlotEmpty(p)){
+                            System.out.println();
+                        }
+                    }*/
+                    
+                //}
+               // else {
+                    int done = 0;
+                    //while(done == 0){
+                        for(int j = 0; done == 0 && j < rooms.size(); j++){
+                            //System.out.println(rooms.get(j));
+                            for(int jj = 0; done == 0 && jj < possibleTimes.size(); jj++){
+                                //System.out.println(possibleTimes.get(jj));
+                                if(rooms.get(j).isSlotEmpty(possibleTimes.get(jj))){
+                                   // System.out.println("got a match");
+                                    done++;
+                                    rooms.get(j).useSlot(possibleTimes.get(jj));
+                                    y.setClassroom(rooms.get(j).getRoomNum());
+                                    y.setTime(possibleTimes.get(jj));
+                                }    
+                            }
+                        }
+                        //done++;
+                   // }
+               // }
+            } 
+            Parser.printList(c);
         }
+        
+        //Parser.printList(t);
     }
     
     private List matchPref(String time){
         List<String> s = new ArrayList();
-
+        System.out.println(time);
         for(String i:timeSlots){
             String[] tmp = i.split(":| ");
             //for(String x:tmp)
-            //    System.out.print(x + "; ");
-            if(tmp[8].equalsIgnoreCase("am") && time.equalsIgnoreCase("Morning")){
+                //System.out.print(x + "; ");
+            if(tmp[6].equalsIgnoreCase("am") && time.equalsIgnoreCase("Morning")){
                 s.add(i);
             }
-            if(tmp[8].equalsIgnoreCase("pm") && time.equalsIgnoreCase("Afternoon") && Integer.parseInt(tmp[5])<6){
+            else if(tmp[6].equalsIgnoreCase("pm") && time.equalsIgnoreCase("Afternoon") && Integer.parseInt(tmp[4])<6){
                 s.add(i);
             }
-            if(tmp[8].equalsIgnoreCase("pm") && time.equalsIgnoreCase("Evening") && Integer.parseInt(tmp[5])>6){
+            else if(tmp[6].equalsIgnoreCase("pm") && time.equalsIgnoreCase("Evening") && Integer.parseInt(tmp[4])>6){
                 s.add(i);
             }
-            if(tmp[9].equalsIgnoreCase("MW") && time.equalsIgnoreCase("MW")){
+            else if(tmp[7].equalsIgnoreCase("MW") && time.equalsIgnoreCase("MW")){
                 s.add(i);
             }
-            if(tmp[9].equalsIgnoreCase("TR") && time.equalsIgnoreCase("TR")){
+            else if(tmp[7].equalsIgnoreCase("TR") && time.equalsIgnoreCase("TR")){
                 s.add(i);
             }
         }
@@ -179,7 +206,7 @@ public class CourseScheduler {
     }
     
     // create time slots (military time) based on duration of classes (dur), and time between classes (gap)
-    private void makeTimeSlots(){
+    private void makeTimeSlots(List<Classroom> rooms){
         int startOfDay = 805;
         int totalClassTime = duration + gap;
         int totalDayTime = 720; // Because there are MW and TR classes
@@ -189,11 +216,11 @@ public class CourseScheduler {
         
         // Find time slots
         for(int i = 0; i < numberOfTimeSlots; i++){
-            String amORpm1 = " am";
-            String amORpm2 = " am";
+            String amORpm1 = "am";
+            String amORpm2 = "am";
             int slot1 = startOfDay + ((int)((totalClassTime * i)/60))*(100) + (totalClassTime * i)%60;;
             if(slot1%100 >= 60) slot1+=40;
-            if(slot1 >= 1200) { amORpm1 = " pm"; amORpm2 = " pm"; slot1-=1200;}
+            if(slot1 >= 1200) { amORpm1 = "pm"; amORpm2 = "pm"; slot1-=1200;}
             
             
             int slot2 = slot1 + ((int)((totalClassTime)/60))*(100) + ((totalClassTime)%60);
@@ -210,13 +237,20 @@ public class CourseScheduler {
             timeSlots.add(ele);
             //System.out.println(ele);
         }
+        for(Classroom c: rooms){
+            //System.out.println("*********************");
+            c.setTimeSlots(courseMeetingTimes);
+        }
+        System.out.println("Available Times");
+        Parser.printList(timeSlots);
+        //return rooms;
     }
     
     /* Modified by Victoria Mitchell */
     public static void assignClassroomPreferences(List<Course> courses, List<String> prefs, List<Teacher> teachers, String[] courseMeetingTimes, 
             List<Classroom> classrooms) {
         for(int i = 0; i < prefs.size(); i++) {
-            System.out.println(prefs.get(i));
+            //System.out.println(prefs.get(i));
             for(Course ele: courses) {
                 if (prefs.get(i).equals(ele.name)) {
                     ele.setBuilding(prefs.get(++i));
@@ -228,7 +262,7 @@ public class CourseScheduler {
                     for (Teacher iTeacher : teachers){
                         for (String teacherCourse : iTeacher.teachableCourses){
                             if (ele.name.equalsIgnoreCase(teacherCourse)) {
-                                ele.setProf(iTeacher.getName());
+                                ele.setProf(iTeacher.name);
                             }                
                         }
                     }*/
@@ -237,7 +271,7 @@ public class CourseScheduler {
                             ele.time = courseMeetingTimes[iTimeSlot];
                             for (Classroom thisRoom : classrooms){
                                 if (thisRoom.name.equals(ele.getClassroom())){
-                                    thisRoom.timeSlot[iTimeSlot] = false;
+                                   // thisRoom.timeSlot[iTimeSlot] = false;
                                 }
                             }                            
                             ele.assigned = true;
