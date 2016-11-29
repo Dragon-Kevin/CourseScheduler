474eabf9f45bffaa2efba6142556005ebb42d215
Mykol00 <Mykol00@gmail.com>
2016-11-29 16:11:31 -0600

    commented out that for loop.

--- src/course_scheduler_beta/CourseScheduler.java
+++ src/course_scheduler_beta/CourseScheduler.java
@@ -28,6 +28,7 @@ public class CourseScheduler {
         this.gap = db.getDuration()[1];
         this.db = db;
         //System.out.println(db.getDuration()[0]);
+        System.out.println("291 is = " + db.getSingleCourse(10099));
         textArea = errorTextArea;
         //scheduleCourses();        //since updateTable already calls this, and configureTable calls updateTable, this was unecesscary
     }
@@ -44,19 +45,18 @@ public class CourseScheduler {
         textArea.clear();  //this needs to be called to clear out error messages, but for some reason after init run the error messages aren't firing (that if inst triggering)
         
         List <Course> c = db.getCourses(null, null);   //clears all time data
-        for(Course ele: c){                            //the null string problem is creeping into the
+       /* for(Course ele: c){                            //the null string problem is creeping into the
             ele.setDays(null);                         //    error message display stuff
             ele.setsTime(null);                        //doing this made the messages actually appear correctly 
             ele.seteTime(null);                        //   in the sense of the if statement in nonPrefs and teacherPrefs
             ele.setClassroom(null);                    //   actually fire and print the error messages 
             ele.setBuilding(null);                     //take note that courses without teachers do NOT display errors  
             db.alterCourse(ele);                       //   i guess that since a course without a teacher cannot be scheduled
-        }                                              //I do not know if this clobbers anything important, so let me know if im destroying stuff that you need for your checking
-        
+        }*/                                              //I do not know if this clobbers anything important, so let me know if im destroying stuff that you need for your checking
         // Make the time slots for assigning classes
         makeTimeSlots(classrooms);
         // Try to assign teachers with time preferences first
-        teacherPrefs(classrooms);
+        teacherPrefs(classrooms);        
         // Assign teachers without preferences next
         nonPrefs(classrooms); 
     }
@@ -75,9 +75,10 @@ public class CourseScheduler {
                 //System.out.println(t.getEmptySlots()[i]);
             //}
             for(Course c: courses){
-                
+                //System.out.println(c);
                 if(c.getClassroom() != null && !c.getClassroom().equalsIgnoreCase("null") && c.getClassroom().length() > 0){
                     Classroom r = new Classroom();
+                    //System.out.println(r);
                     for(Classroom i: rooms){
                         if(i.getRoomNum().equalsIgnoreCase(c.getClassroom())){
                             r = i;
@@ -105,13 +106,21 @@ public class CourseScheduler {
                         for(int j = 0; done == 0 && j < timeSlots.size(); j++){
                             //System.out.println("room " + i + "slot " + j);
                             if(rooms.get(i).isSlotEmpty(timeSlots.get(j)) && t.isSlotEmpty(timeSlots.get(j))){
-                                
-                                done++;
-                                rooms.get(i).useSlot(timeSlots.get(j));
-                                t.useSlot(timeSlots.get(j));
-                                c.setClassroom(rooms.get(i).getRoomNum());
-                                c.setTime(timeSlots.get(j));
-                                c.setBuilding(rooms.get(i).getBuildingName());
+                                int match = 0;
+                                if(rooms.get(i).getBuildingName().equalsIgnoreCase("Technology Hall") && c.getDepartment().equalsIgnoreCase("Computer Science")){
+                                    match++;
+                                }
+                                else if(rooms.get(i).getBuildingName().equalsIgnoreCase("Engineering Building") && c.getDepartment().equalsIgnoreCase("ECE")){
+                                    match++;
+                                }
+                                if(match > 0){
+                                    done++;
+                                    rooms.get(i).useSlot(timeSlots.get(j));
+                                    t.useSlot(timeSlots.get(j));
+                                    c.setClassroom(rooms.get(i).getRoomNum());
+                                    c.setTime(timeSlots.get(j));
+                                    c.setBuilding(rooms.get(i).getBuildingName());
+                                }
                             }    
                         }
                     }
@@ -143,7 +152,7 @@ public class CourseScheduler {
             t.setTimeSlots(possibleTimes.toArray(new String[0]));
             //Parser.printList(possibleTimes);
             for(Course c:courses){
-                                                                                //added below check to not fire on classroom actually holding string "null" 
+                                                                               //added below check to not fire on classroom actually holding string "null" 
                 if(c.getClassroom() != null && c.getClassroom().length() > 0 && !c.getClassroom().equals("null")){
                     Classroom r = new Classroom();
                     for(Classroom i: rooms){
@@ -168,18 +177,27 @@ public class CourseScheduler {
                     for(int i = 0; done == 0 && i < rooms.size(); i++){
                         for(int j = 0; done == 0 && j < possibleTimes.size(); j++){
                             if(rooms.get(i).isSlotEmpty(possibleTimes.get(j)) && t.isSlotEmpty(possibleTimes.get(j))){
-                                done++;
-                                rooms.get(i).useSlot(possibleTimes.get(j));
-                                t.useSlot(possibleTimes.get(j));
-                                c.setClassroom(rooms.get(i).getRoomNum());
-                                c.setTime(possibleTimes.get(j));
-                                c.setBuilding(rooms.get(i).getBuildingName());
+                                int match = 0;
+                                if(rooms.get(i).getBuildingName().equalsIgnoreCase("Technology Hall") && c.getDepartment().equalsIgnoreCase("Computer Science")){
+                                    match++;
+                                }
+                                else if(rooms.get(i).getBuildingName().equalsIgnoreCase("Engineering Building") && c.getDepartment().equalsIgnoreCase("ECE")){
+                                    match++;
+                                }
+                                if(match > 0){
+                                    done++;
+                                    rooms.get(i).useSlot(possibleTimes.get(j));
+                                    t.useSlot(possibleTimes.get(j));
+                                    c.setClassroom(rooms.get(i).getRoomNum());
+                                    c.setTime(possibleTimes.get(j));
+                                    c.setBuilding(rooms.get(i).getBuildingName());
+                                }
                             }    
                         }
                     }
                     if(done == 0){
                         c.setBuilding("null");
-                        textArea.appendText(c.getName() + " could not be scheduled. Teacher time preference conlict!\n");
+                        textArea.appendText(c.getName() + " could not be scheduled. Teacher time preference conflict!\n");
                     }
                 }
                 db.alterCourse(c);
